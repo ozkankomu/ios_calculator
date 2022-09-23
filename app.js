324ee57cb8f81301da14cd01c7b35847ef3d6f5d
ozkankomu <ozkankomu35@gmail.com>
2022-09-23 19:05:57 +0300

    "js codes added"

--- app.js
+++ app.js
@@ -1,5 +1,113 @@
+//!  calculator      //
+
+const currentDisplay = document.querySelector(".current-display");
+const keys = document.querySelector(".numbers");
+let previousDisplay = "0";
+let displayValue = "0";
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
+
+updateDisplay();
+
+function updateDisplay() {
+  currentDisplay.innerText = displayValue;
+}
+
+keys.addEventListener("click", (e) => {
+  const element = e.target;
+  if (!element.classList.contains("button")) return;
+
+  const value = element.innerText;
+  console.log(value);
+
+  switch (value) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case "=":
+    case "%":
+      handleOperator(value);
+      break;
+
+    case ".":
+      inputDecimal();
+      break;
+    case "AC":
+      clear();
+      break;
+    default:
+      inputNumber(element.innerText);
+  }
+  updateDisplay();
+
+  // if (element.classList.contains("function")) {
+  //   return;
+  // }
+
+  // console.log("number", element.innerText);
+});
+
+//!  //////////   functions  ////// ///////
+
+function inputNumber(num) {
+  if (waitingForSecondValue) {
+    previousDisplay.innerText = displayValue;
+    displayValue = num;
+    waitingForSecondValue = false;
+  } else {
+    displayValue = displayValue === "0" ? num : displayValue + num;
+  }
+}
+
+function inputDecimal() {
+  if (!displayValue.includes(".")) {
+    displayValue += ".";
+  }
+}
+
+function clear() {
+  displayValue = "0";
+}
+
+function handleOperator(nextOperator) {
+  const value = parseFloat(displayValue);
+
+  if (operator && waitingForSecondValue) {
+    operator = nextOperator;
+    return;
+  }
+  if (firstValue === null) {
+    firstValue = value;
+  } else if (operator) {
+    const result = calculate(firstValue, value, operator);
+    displayValue = `${parseFloat(result.toFixed(10))}`;
+    firstValue = result;
+  }
+  waitingForSecondValue = true;
+  operator = nextOperator;
+}
+
+function calculate(first, second, operator) {
+  if (operator === "+") {
+    return first + second;
+  } else if (operator === "*") {
+    return first * second;
+  } else if (operator === "-") {
+    return first - second;
+  } else if (operator === "/") {
+    return first / second;
+  } else if (operator === "%") {
+    return (first * second) / 100;
+  }
+  return second;
+}
+
+//!  saat    section   ///////
+
 // DOM Elements
-const hourEl = document.querySelector(".hour");
+let hourEl = document.querySelector(".hour");
 const minuteEl = document.querySelector(".minute");
 const displayEl = document.querySelector(".display");
 const acEl = document.querySelector(".ac");
